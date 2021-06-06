dde10bff30fbe16570bf3db1cd1405d151cb9ed2
HectorS04 <84256867+HectorS04@users.noreply.github.com>
2021-06-06 16:57:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-let menuStatus = true;
 let menu = document.getElementById("menu");
 let btnmenu = document.getElementById("btn-menu");
 let iconmenu = document.getElementById("icon-menu");
+let shadow = document.getElementById("shadow");
 
 btnmenu.addEventListener("click",algo);
-
+shadow.addEventListener("click",algo)
 
 function algo(){
    if(menuStatus){
@@ -14,13 +14,15 @@ function algo(){
       menu.classList.add('menu-active');
       iconmenu.classList.remove('fa-bars');
       iconmenu.classList.add('fa-times');
+      shadow.classList.add('shadow');
+      
    }else{
       menuStatus = true;
       menu.classList.remove('menu-active');
       menu.classList.add('menu-noactive');
       iconmenu.classList.remove('fa-times');
       iconmenu.classList.add('fa-bars');
+      shadow.classList.remove('shadow');
    }
    // console.log(x);
 }
-// alert("po");
